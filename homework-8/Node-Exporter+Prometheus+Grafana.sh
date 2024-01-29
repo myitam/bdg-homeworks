42febf241360ebfc536e4a8a89fe5a0a70ebd3ff
Artur Shakhkyan <ashakhkyan@gmail.com>
2024-01-29 06:02:48 +0000

    adding conditions

--- homework-8/Node-Exporter+Prometheus+Grafana.sh
+++ homework-8/Node-Exporter+Prometheus+Grafana.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
-./Node-Exporter.sh
-./prometheus.sh
-./grafana.sh
+#!/bin/bash
+
+
+if systemctl --all --type service | grep -q "prometheus";then
+
+else
+   ./prometheus.sh
+fi
+
+if systemctl --all --type service | grep -q "Node-Exporter";then
+
+else
+   ./Node-Exporter.sh
+fi
+
+if systemctl --all --type service | grep -q "grafana";then
+
+else
+   ./grafana.sh
+   
+fi
+
